fe3949b44df18d598208be5de15537a85c1928c4
Alex Whedbee <awhedbee22@gmail.com>
2016-06-29 14:42:10 -0700

    New Layout
    Needs more tweaks

---
+++ js/init.js
@@ -0,0 +1,25 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+// Smooth Scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 64
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
